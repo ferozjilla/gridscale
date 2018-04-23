f7227020101b850f42cbe496d1bbc123f4d2dd36
Feroz Jilla <ferozjilla@gmail.com>
2018-04-23 11:43:18 +0100

    Implement basic job submission in gridscale
    - a simple shell command is run. Thus, todo file upload and result
      download
    - configuration is assumed by the functions. Thus, need to devise a way
      to pass in the configuration.
    - program crashes with a non-zero exit code when azure batch sdk
      instructions fail. Need to handle exceptions more gracefully.
    
    [#156658500]

--- examples/azure/src/main/scala/gridscale/azure/TestAzure.scala
+++ examples/azure/src/main/scala/gridscale/azure/TestAzure.scala
@@ -1,8 +1,11 @@
 package gridscale.azure
 
 import java.io.{ ByteArrayOutputStream, OutputStream }
+import java.util
 
-import com.microsoft.azure.batch.protocol.models.TaskAddParameter
+import com.microsoft.azure.batch.DetailLevel
+import com.microsoft.azure.batch.DetailLevel.Builder
+import com.microsoft.azure.batch.protocol.models._
 
 object TestAzure extends App {
 
@@ -11,47 +14,83 @@ object TestAzure extends App {
   import com.microsoft.azure.batch.protocol.models.{ CloudPool, ImageReference, VirtualMachineConfiguration, PoolInformation }
 
   val client: BatchClient = getBatchClient()
-  val poolId = "test-pool-2"
-  val jobId = "test-job-2"
-  val taskId = "test-task-2"
+  val poolId = "pool-test"
+  val jobId = "job-test"
+  val taskId1 = "task-test-1"
+  val taskId2 = "task-test-2"
+  val taskId3 = "task-test-3"
 
   createPoolIfNotExists(client, poolId)
   createJob(client, poolId, jobId)
-  addTaskToJob(client, jobId, taskId)
-  waitForTaskCompletion(client, jobId, taskId)
-  // Uncomment after waitForTaskCompletion is implemented
-  //printTaskOutput(client, jobId, taskId)
+  addTaskToJob(client, jobId, taskId1)
+  addTaskToJob(client, jobId, taskId2)
+  addTaskToJob(client, jobId, taskId3)
+  waitForJobCompletion(client, jobId, 1000)
+  printTaskOutput(client, jobId, taskId1)
+  printTaskOutput(client, jobId, taskId2)
+  printTaskOutput(client, jobId, taskId3)
   deletePool(client, poolId)
 
   // Get a batch client
   def getBatchClient(): BatchClient = {
-    val batchUri = sys.env("BATCH_URI")
-    val batchAccount = sys.env("BATCH_ACCOUNT")
-    val batchKey = sys.env("BATCH_KEY")
+    println("Creating Batch Client")
+    val batchUri = sys.env("AZURE_BATCH_ENDPOINT")
+    val batchAccount = sys.env("AZURE_BATCH_ACCOUNT")
+    val batchKey = sys.env("AZURE_BATCH_ACCESS_KEY")
     val creds: BatchSharedKeyCredentials = new BatchSharedKeyCredentials(batchUri, batchAccount, batchKey)
     return BatchClient.open(creds)
   }
 
   // Create a pool
   def createPoolIfNotExists(client: BatchClient, poolId: String): CloudPool = {
-    // Assume pool properties
-    val poolVMSize = "STANDARD_A1"
-    val poolVMCount = 1
-
-    val osPublisher = "Canonical"
-    val osOffer = "UbuntuServer"
-    val skuId = "16.04-LTS"
-    val nodeAgentSkuId = "batch.node.ubuntu 16.04"
-    val imgRef = new ImageReference().withOffer(osOffer).withPublisher(osPublisher).withSku(skuId)
-    val configuration: VirtualMachineConfiguration = new VirtualMachineConfiguration()
-    configuration.withNodeAgentSKUId(nodeAgentSkuId).withImageReference(imgRef)
-    if (!client.poolOperations().existsPool(poolId)) {
-      client.poolOperations().createPool(poolId, poolVMSize, configuration, poolVMCount)
+    println("Creating Pool: " + poolId)
+    // Return pool if already exists
+    if (client.poolOperations().existsPool(poolId)) {
+      return client.poolOperations().getPool(poolId)
     }
 
-    // TODO: Add better blocking
-    while (!client.poolOperations().existsPool(poolId)) {
-      Thread.sleep(30000)
+    // Assume virtual machine properties
+    val POOL_DISPLAY_NAME = "Test Pool"
+    val POOL_VM_SIZE = "STANDARD_A1"
+    val POOL_VM_COUNT = 1
+    val POOL_OS_PUBLISHER = "Canonical"
+    val POOL_OS_OFFER = "UbuntuServer"
+    val POOL_SKUID = "16.04-LTS"
+    val POOL_STEADY_TIMEOUT_IN_SECONDS = 5 * 60 * 1000
+    val NODE_AGENT_SKUID = "batch.node.ubuntu 16.04"
+
+    // Create a virtual machine configuration
+    val imgRef = new ImageReference().withOffer(POOL_OS_OFFER).withPublisher(POOL_OS_PUBLISHER).withSku(POOL_SKUID)
+    val poolVMConfiguration = new VirtualMachineConfiguration()
+      .withNodeAgentSKUId(NODE_AGENT_SKUID)
+      .withImageReference(imgRef)
+
+    // Create object with pool parameters
+    val poolAddParameter: PoolAddParameter = new PoolAddParameter()
+      .withId(poolId)
+      .withDisplayName(POOL_DISPLAY_NAME)
+      .withTargetDedicatedNodes(POOL_VM_COUNT)
+      .withVmSize(POOL_VM_SIZE)
+      .withVirtualMachineConfiguration(poolVMConfiguration)
+
+    // Create pool if it does not exist
+    if (!client.poolOperations().existsPool(poolId)) {
+      client.poolOperations().createPool(poolAddParameter)
+    }
+
+    // Wait until pool created within bounds
+    val startTime = System.currentTimeMillis()
+    var timeElapsed = 0L
+    var steady = false
+    var pool: CloudPool = client.poolOperations().getPool(poolId)
+
+    while (pool.allocationState() != AllocationState.STEADY) {
+      if (timeElapsed > POOL_STEADY_TIMEOUT_IN_SECONDS) {
+        throw new Exception("Could not create the pool within time")
+      }
+      pool = client.poolOperations().getPool(poolId)
+      Thread.sleep(30 * 1000)
+      timeElapsed = System.currentTimeMillis() - startTime
     }
 
     return client.poolOperations().getPool(poolId)
@@ -59,6 +98,7 @@ object TestAzure extends App {
 
   // Create job
   def createJob(client: BatchClient, poolId: String, jobId: String): Unit = {
+    println("Creating Job: " + jobId)
     val poolInfo = new PoolInformation
     poolInfo.withPoolId(poolId)
     client.jobOperations.createJob(jobId, poolInfo)
@@ -66,17 +106,44 @@ object TestAzure extends App {
 
   // Add task to job
   def addTaskToJob(client: BatchClient, jobId: String, taskId: String): Unit = {
+    println("Adding task " + taskId + " to job " + jobId)
     val task: TaskAddParameter = new TaskAddParameter
     task.withId(taskId).withCommandLine(String.format("echo idea2"))
     client.taskOperations().createTask(jobId, task)
   }
 
-  //TODO: Implement
-  def waitForTaskCompletion(client: BatchClient, jobId: String, taskId: String): Unit = {
+  // Waits for all tasks in a job to complete
+  def waitForJobCompletion(client: BatchClient, jobId: String, expiryTimeInSeconds: Integer): Boolean = {
+    println("Waiting for job to finish: " + jobId)
+    val startTime = System.currentTimeMillis()
+    var elapsedTime = 0L
+
+    while (elapsedTime < expiryTimeInSeconds * 1000) {
+      val tasks: util.List[CloudTask] = client.taskOperations().listTasks(jobId, new DetailLevel.Builder().withSelectClause("id, state").build())
+      println("Total tasks: " + tasks.size())
+      var allComplete = true
+      // TODO: What is a good Scala way for something like this
+      for (i ← 0 until tasks.size()) {
+        if (tasks.get(i).state() != TaskState.COMPLETED) {
+          allComplete = false
+        }
+      }
+
+      if (allComplete) {
+        println("All jobs complete")
+        return true
+      }
+
+      Thread.sleep(10 * 1000)
+      elapsedTime = System.currentTimeMillis() - startTime
+    }
+
+    return false
   }
 
   // Get stdout and stderr
   def printTaskOutput(client: BatchClient, jobId: String, taskId: String): Unit = {
+    println("Fetching stdout")
     var stream = new ByteArrayOutputStream
     val file = "stdout.txt"
     client.fileOperations().getFileFromTask(jobId, taskId, file, null, stream)
@@ -87,6 +154,7 @@ object TestAzure extends App {
 
   // Delete pool
   def deletePool(client: BatchClient, poolId: String): Unit = {
+    println("Deleting Pool: " + poolId)
     client.poolOperations().deletePool(poolId)
   }
 }
